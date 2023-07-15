0112035fbc364c339a4a84ff4c0b494c2cc4c333
Tatyana Shevchenko <tatyanashevchenko@10.0.0.14>
2023-07-15 09:12:23 +0200

    change frontend port

--- routes/items.js
+++ routes/items.js
@@ -1,6 +1,6 @@
 // import required essentials
 const express = require("express");
-const { faker } = require('@faker-js/faker');
+const { faker } = require("@faker-js/faker");
 
 // create new router
 const router = express.Router();
@@ -9,15 +9,15 @@ const router = express.Router();
 let data = [];
 // Generate 10000 mock objects
 for (let i = 1; i <= 10000; i++) {
-    const object = {
-      id: i,
-      subject: `Ticket ${i}`,
-      priority: faker.helpers.arrayElement(['low', 'medium', 'high']),
-      status: faker.helpers.arrayElement(['to do', 'in progress', 'done']),
-      description: faker.lorem.sentence()
-    };
-    data.push(object);
-  }
+  const object = {
+    id: i,
+    subject: `Ticket ${i}`,
+    priority: faker.helpers.arrayElement(["low", "medium", "high"]),
+    status: faker.helpers.arrayElement(["to do", "in progress", "done"]),
+    description: faker.lorem.sentence(),
+  };
+  data.push(object);
+}
 
 // get all tickets
 router.get("/", function (req, res) {
@@ -37,23 +37,23 @@ router.get("/:id", function (req, res) {
 });
 
 // Create a new ticket
-router.post('/', function (req, res) {
-    const object = req.body;
-    data.push(object);
-    res.status(201).json(data);
-  });
+router.post("/", function (req, res) {
+  const object = req.body;
+  data.push(object);
+  res.status(201).json(data);
+});
 
 // Update an existing object
-router.put('/:id', (req, res) => {
-    const id = req.params.id;
-    const objectIndex = data.findIndex(obj => obj.id == id);
-  
-    if (objectIndex === -1) {
-      res.status(404).json({ error: 'Object not found' });
-    } else {
-      data[objectIndex] = req.body;
-      res.json(data[objectIndex]);
-    }
-  });
+router.put("/:id", (req, res) => {
+  const id = req.params.id;
+  const objectIndex = data.findIndex((obj) => obj.id == id);
+
+  if (objectIndex === -1) {
+    res.status(404).json({ error: "Object not found" });
+  } else {
+    data[objectIndex] = req.body;
+    res.json(data[objectIndex]);
+  }
+});
 
 module.exports = router;
--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const itemsRouter = require("./routes/items");
 const app = express();
 
 app.use(express.json());
-app.use(cors({ origin: "http://localhost:8100" }));
+app.use(cors({ origin: "http://localhost:3006" }));
 app.use("/items", itemsRouter);
 
 // default URL to API
